0d16685f5468f84fb9eb5fab77661f6c0f4739d9
Eric Magar <emagar@gmail.com>
2018-10-17 14:00:58 -0500

    Edits typos in ay incumbents

--- code/incumbents.r
+++ code/incumbents.r
@@ -6,7 +6,6 @@ inc <- read.csv(file = "aymu1985-present.incumbents.csv", stringsAsFactors = FAL
 
 head(inc)
 
-
 inc$race.prior <- NA
 
 # drop observations
@@ -34,6 +33,29 @@ for (e in 1:32){
     inc[sel.e,] <- inc.e
 }
 
+# simplify parties
+inc$win2 <- inc$win
+sel <- grep("ci_|^ci$|c-i-|ind_|eduardo|luis|oscar|indep", inc$win2, ignore.case = TRUE)
+inc$win2[sel] <- "indep"
+sel <- grep("pan-", inc$win2, ignore.case = TRUE)
+inc$win2[sel] <- "pan-coal"
+sel <- grep("pri-", inc$win2, ignore.case = TRUE)
+inc$win2[sel] <- "pri-coal"
+sel <- grep("prd-", inc$win2, ignore.case = TRUE)
+inc$win2[sel] <- "prd-coal"
+sel <- grep("morena-|-morena", inc$win2, ignore.case = TRUE)
+inc$win2[sel] <- "morena-coal"
+sel <- grep("pvem-", inc$win2, ignore.case = TRUE)
+inc$win2[sel] <- "pvem-coal"
+sel <- grep("mas|paz|pcp|phm|pmr|ppg|psd1|psi|pudc|pup|via_", inc$win2, ignore.case = TRUE)
+inc$win2[sel] <- "local"
+sel <- grep("pt1", inc$win2, ignore.case = TRUE)
+inc$win2[sel] <- "pt"
+#
+sel <- which(inc$yr==2018)
+table(inc$win2[sel])
+
+
 table(inc$race.prior)
 sel <- grep("out", inc$race.prior, ignore.case = TRUE) 
 inc$race.prior[sel] <- "Frosh-open-seat"
@@ -49,10 +71,20 @@ inc$race.prior[sel] <- "Frosh-who-ousted"
 sel <- which(inc$race.prior=="" & inc$incumbent!="") #cases still unknown
 #inc$incumbent[sel]
 inc$race.prior[sel] <- "Frosh-unknown"
+sel <- which(inc$race.prior=="") #cases still unknown
+inc$race.prior[sel] <- "Yet-to-know"
 
-table(inc$edon[inc$yr==2018], inc$race.prior[inc$yr==2018])
-      table(inc$race.prior[inc$yr==2018])
-round(table(inc$race.prior[inc$yr==2018]) / sum(as.numeric(inc$yr==2018)),2)
+
+sel <- which(inc$yr==2018 & inc$edon!=9 & inc$edon!=21)
+table(inc$edon[sel], inc$race.prior[sel])
+      table(inc$race.prior[sel])
+round(table(inc$race.prior[sel]) / length(sel),2)
+
+
+
+
 
 head(inc)
+
+I still need to collect data for Chiapas, Michoacán, and Sinaloa. 
 
