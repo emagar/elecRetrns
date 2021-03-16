56fbbe87fa05e4adea71d1eafcdd321ed6fc33ac
Eric Magar <emagar@gmail.com>
2021-03-15 21:04:59 -0600

    Moved xport-function.r tu data/useful-functions

--- code/extract-state-yr-mu-returns.r
+++ code/extract-state-yr-mu-returns.r
@@ -3,14 +3,26 @@
 ## Output saved in ./xport/ directory with one column for each party.                       ##
 ##############################################################################################
 
-# Edit your path to code/
-setwd("/home/eric/Desktop/MXelsCalendGovt/elecReturns/code")
+# Edit your path to aymu1989-present.coalAgg.csv
+setwd("/home/eric/Desktop/MXelsCalendGovt/elecReturns/data")
+
+# if my machine use scripts in disk
+pth <- ifelse (Sys.info()["user"] %in% c("eric", "magar"),
+    "~/Dropbox/data/useful-functions",
+    "https://raw.githubusercontent.com/emagar/useful-functions/master"
+    )
+#pth <- "https://raw.githubusercontent.com/emagar/useful-functions/master" # debug
+
 
 # Reads xport function
-source("xport-function.r")
+pth <- paste(pth, "xport-function.r", sep = "/")
+source(pth)
+
+## # if no internet connection, source from disk version
+## source("~/dropbox/data/useful-functions/xport-function.r")
 
 # Usage
-xport(e=2, y=2019)
+xport(e=2, y=2019) # where e is edon or edo
 
-# or be prompted for e and y
-xport()
+# or be prompted for a year
+xport(e=2)
